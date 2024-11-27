48b5ff311ed8c330f1feb821bfda51e64e6a2beb
MUHAMMAD-ROKY <rokyksy@gmail.com>
2024-11-27 16:15:17 +0600

    multi page add system js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-<script>
-    window.alert("Javascript series"); //this function have to see window
-    document.write("I am a fontend developer"); // this function have to see html documents 
-    console.log("Hello World"); //this function have to see browser base i means console base
-</script>
+window.alert("Javascript series"); //this function have to see window
+document.write("I am a fontend developer"); // this function have to see html documents 
+console.log("Hello World"); //this function have to see browser base i means console base
