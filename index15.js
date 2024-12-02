4b456e360aecb651c1b67ff1c67a21ef65dd22fa
MUHAMMAD-ROKY <rokyksy@gmail.com>
2024-12-02 12:55:29 +0600

    ternary operator

---
+++ index15.js
@@ -0,0 +1,17 @@
+var num1 = Number(prompt("Enter your number:"));    //two condition
+var result = num1 > 0 ? "positive" : "negative";
+console.log(result);
+
+
+
+var num1 = Number(prompt("Enter your number:"));
+var result = num1 > 0 ? console.log("positive") : console.log("negative");
+console.log(result);
+
+
+
+//two or more condition
+var number1 = Number(prompt("Please Enter your number:"));
+var results = number1 > 0 ? "positive" : number1 < 0 ? "Negative" : "Zero";
+console.log(results);
+
