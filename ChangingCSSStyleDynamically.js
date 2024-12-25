92c5caf5e754d66110ae191d02ed9aa478ee65fd
MUHAMMAD-ROKY <rokyksy@gmail.com>
2024-12-25 16:06:22 +0600

    changing css style Dynamically

---
+++ ChangingCSSStyleDynamically.js
@@ -0,0 +1,8 @@
+function addStyle(){
+    var myVar = document.querySelector("#paraId");
+    myVar.classList.add("paraStyle");
+}
+function RemoveStyle(){
+    var myVar = document.querySelector("#paraId");
+    myVar.classList.remove("paraStyle");
+}
