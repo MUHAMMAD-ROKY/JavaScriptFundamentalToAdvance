405c2f3d4ebea5917f4593791ce5e712c67b4059
MUHAMMAD-ROKY <rokyksy@gmail.com>
2025-01-02 14:49:39 +0600

    variable function ES6 decleration

---
+++ variableFunctionES6.js
@@ -0,0 +1,15 @@
+/*
+//es5 function decelaration
+function add(x,y){
+    var sum = x+y;
+    console.log(sum);
+}
+add(5,10);
+*/
+
+//es6 function decelaration   updated system
+const add = (x,y)=>{
+    let sum = x+y;
+    console.log(sum);
+}
+add(5,10);
