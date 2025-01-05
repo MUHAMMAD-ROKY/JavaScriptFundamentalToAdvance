804d004f44f9e2a4a65badb15f737fb5eaeb5937
MUHAMMAD-ROKY <rokyksy@gmail.com>
2025-01-05 17:29:59 +0600

    ES6ModuleAndClass

---
+++ ES6ModuleAndClass/index.js
@@ -0,0 +1,9 @@
+/*
+import {setText, text} from "./myModules.js";
+console.log(text);
+setText("Good bye Bangladesh");
+console.log(text);
+*/
+
+import {message} from "./myModules.js";
+console.log(message);
---
+++ ES6ModuleAndClass/myModules.js
@@ -0,0 +1,10 @@
+/*
+export let text = "Hello Bangladesh";
+
+export function setText(txt){
+    text = txt;
+}
+*/
+
+let text = "Hello Bangladesh1";
+export{text as message}
