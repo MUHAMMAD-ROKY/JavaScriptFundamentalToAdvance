9a0b4506cf7f163b561b8a2a88ee0e1988c5335a
MUHAMMAD-ROKY <rokyksy@gmail.com>
2025-01-05 15:28:39 +0600

    StringMethods

---
+++ StringMethods.js
@@ -0,0 +1,13 @@
+//startsWith(searchString, position) ---> check a string starts With another string
+const messages1 ="Today is very cool";
+console.log(messages1.startsWith("Today",0));  
+// endsWith(searchString,length)----->check a string ends With another string 
+const messages2 ="Today is very cool";
+console.log(messages2.endsWith("cool"));   
+// includes(searchString, position)---->check if a string  contains another string
+const messages3 ="Today is very cool";
+console.log(messages3.includes("cool")); 
+//all these method are case sensitive
+
+
+
