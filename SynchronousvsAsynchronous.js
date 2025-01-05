693e37397e5b4ad0aa47dda3f737b5b751d3ec2d
MUHAMMAD-ROKY <rokyksy@gmail.com>
2025-01-05 17:46:01 +0600

    SynchronousvsAsynchronous

---
+++ SynchronousvsAsynchronous.js
@@ -0,0 +1,25 @@
+//Synchronous programming
+const taskOne = () =>{
+    console.log("Task1");
+}
+const dataLoading = () =>{
+    console.log("Task2 .Data loading");//asynchronous system
+}
+const taskTwo = () =>{
+   setTimeout(dataLoading,2000);    //asynchronous system is not stop processing.It's always run and quickly multi program run.
+}
+const taskThree = () =>{
+    console.log("Task3");
+}
+const taskFour = () =>{
+    console.log("Task4");
+}
+const taskFive = () =>{
+    console.log("Task5");
+}
+
+taskOne();
+taskTwo();
+taskThree();
+taskFour();
+taskFive();
