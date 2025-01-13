b928bb0b31dbd74b5c8e5893f9480a7c36651396
MUHAMMAD-ROKY <rokyksy@gmail.com>
2025-01-14 00:00:34 +0600

    Json Path

---
+++ friends_dataMoreComplex.js
@@ -0,0 +1,18 @@
+const data = require("./friends_dataMoreComplex.json");
+//console.log(data);
+//console.log(data.student[0].friends[0].name);
+//console.log(data.student[1].friends[1].name);
+for(x in data){
+    console.log(x);
+    console.log(data[x]);
+}
+
+
+
+
+
+/*const data1 = {
+    name:"jamal";
+*/
+//console.log(stringy(data1));   only client to send server for work
+//console.log(parse(data1)); only bring server to client for work
